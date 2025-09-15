c2c63279a0a00ad497259f1340d7e0455f562ff2
yaaabs <brianyabut02@gmail.com>
2025-09-15 12:34:12 +0800

    feat: enhance asset loading and improve site performance

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS for all URLs in production
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,44 +22,44 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
 
-    <!-- Mga fcking CSS Files dito-->
-    <link href="css/logreg.css" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="{{ asset('css/logreg.css') }}" rel="stylesheet">
 
     <!-- All CSS -->
     
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
     <!-- Nice Select CSS -->
-    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="{{ asset('css/nice-select.css') }}">
 
     <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
 
     <!-- icofont CSS -->
-    <link rel="stylesheet" href="css/icofont.css">
+    <link rel="stylesheet" href="{{ asset('css/icofont.css') }}">
 
     <!-- Slicknav -->
-    <link rel="stylesheet" href="css/slicknav.min.css">
+    <link rel="stylesheet" href="{{ asset('css/slicknav.min.css') }}">
 
     <!-- Owl Carousel CSS -->
-    <link rel="stylesheet" href="css/owl-carousel.css">
+    <link rel="stylesheet" href="{{ asset('css/owl-carousel.css') }}">
 
     <!-- Datepicker CSS -->
-    <link rel="stylesheet" href="css/datepicker.css">
+    <link rel="stylesheet" href="{{ asset('css/datepicker.css') }}">
 
     <!-- Animate CSS -->
-    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
 
     <!-- Magnific Popup CSS -->
-    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
     
-    <!--  CSS -->
-    <link rel="stylesheet" href="css/home/jquery.fancybox.min.css">
-    <link rel="stylesheet" href="css/normalize.css">
+    <!-- Fancy Box CSS -->
+    <link rel="stylesheet" href="{{ asset('css/home/jquery.fancybox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
  
     <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}">
-    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
 </head>
 
 <body>
@@ -67,49 +67,47 @@
     @yield('content')
 
         <!-- jquery Min JS -->
-        <script src="js/jquery.min.js"></script>
+        <script src="{{ asset('js/jquery.min.js') }}"></script>
 		<!-- jquery Migrate JS -->
-		<script src="js/jquery-migrate-3.0.0.js"></script>
+		<script src="{{ asset('js/jquery-migrate-3.0.0.js') }}"></script>
 		<!-- jquery Ui JS -->
-		<script src="js/jquery-ui.min.js"></script>
+		<script src="{{ asset('js/jquery-ui.min.js') }}"></script>
 		<!-- Easing JS -->
-        <script src="js/easing.js"></script>
+        <script src="{{ asset('js/easing.js') }}"></script>
 		<!-- Color JS -->
-		<script src="js/colors.js"></script>
+		<script src="{{ asset('js/colors.js') }}"></script>
 		<!-- Popper JS -->
-		<script src="js/popper.min.js"></script>
+		<script src="{{ asset('js/popper.min.js') }}"></script>
 		<!-- Bootstrap Datepicker JS -->
-		<script src="js/bootstrap-datepicker.js"></script>
+		<script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
 		<!-- Jquery Nav JS -->
-        <script src="js/jquery.nav.js"></script>
+        <script src="{{ asset('js/jquery.nav.js') }}"></script>
 		<!-- Slicknav JS -->
-		<script src="js/slicknav.min.js"></script>
+		<script src="{{ asset('js/slicknav.min.js') }}"></script>
 		<!-- ScrollUp JS -->
-        <script src="js/jquery.scrollUp.min.js"></script>
+        <script src="{{ asset('js/jquery.scrollUp.min.js') }}"></script>
 		<!-- Niceselect JS -->
-		<script src="js/niceselect.js"></script>
+		<script src="{{ asset('js/niceselect.js') }}"></script>
 		<!-- Tilt Jquery JS -->
-		<script src="js/tilt.jquery.min.js"></script>
+		<script src="{{ asset('js/tilt.jquery.min.js') }}"></script>
 		<!-- Owl Carousel JS -->
-        <script src="js/owl-carousel.js"></script>
+        <script src="{{ asset('js/owl-carousel.js') }}"></script>
 		<!-- counterup JS -->
-		<script src="js/jquery.counterup.min.js"></script>
+		<script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
 		<!-- Steller JS -->
-		<script src="js/steller.js"></script>
+		<script src="{{ asset('js/steller.js') }}"></script>
 		<!-- Wow JS -->
-		<script src="js/wow.min.js"></script>
+		<script src="{{ asset('js/wow.min.js') }}"></script>
 		<!-- Magnific Popup JS -->
-		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
 		<!-- Counter Up CDN JS -->
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
 		<!-- Bootstrap JS -->
-		<script src="js/bootstrap.min.js"></script>
+		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 		<!-- Main JS -->
-		<script src="js/main.js"></script>
+		<script src="{{ asset('js/main.js') }}"></script>
 
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="path/to/jquery.magnific-popup.min.js"></script>
-
-        <script src="js/home/jquery.fancybox.min.js"></script>
+        <!-- Fancy Box JS -->
+        <script src="{{ asset('js/home/jquery.fancybox.min.js') }}"></script>
 </body>
 </html>
